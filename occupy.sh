56200cf5765554b3d261b312976c5ed38dfa9768
Petr Prokhorenkov <eeight@yandex-team.ru>
2012-04-10 22:47:52 +0400

    Fix occupy.

--- occupy.sh
+++ occupy.sh
@@ -7,12 +7,14 @@ echo Installing oh-my-zsh
 ssh "$host" "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh"
 
 echo Cloning dotfiles
-ssh "$host" "git clone github.com/eeight/dotfiles.git:.dotfiles .dotfiles"
+ssh "$host" "test -d .dotfiles || git clone git://github.com/eeight/dotfiles.git .dotfiles"
 
 echo Rolling out dotfiles
 ssh "$host" '
-    cd /home/eeight/.dotfiles
-    for f in .*; do
-        ln -sf $f /home/eeight/`basename "$f"`
+    for f in /home/eeight/.dotfiles/.*; do
+        [ "${f%.}" = "$f" ] || continue
+        link=/home/eeight/`basename "$f"`
+        rm -f "$link"
+        ln -sf $f "$link"
     done'
 
